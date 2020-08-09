fa224323fa8e19f24ec7ee9ce0ca2ef876de9fac
Dmitrii Makarov <vensder@gmail.com>
2020-08-09 16:34:17 +1200

    added env var parameter for first domains loading from file,
    removed hardcoded default list,
    updated integration tests

--- cron/app.py
+++ cron/app.py
@@ -40,45 +40,14 @@ is_hash_sum_check_enabled = False
 if "IS_HASH_SUM_CHECK_ENABLED" in environ:
     is_hash_sum_check_enabled = strtobool(environ["IS_HASH_SUM_CHECK_ENABLED"])
 
+is_first_load_from_file_enabled = False
+if "IS_FIRST_LOAD_FROM_FILE_ENABLED" in environ:
+    is_first_load_from_file_enabled = strtobool(
+        environ["IS_FIRST_LOAD_FROM_FILE_ENABLED"])
+
+
 domains_file = "domains.lst"
 domains_set = set()
-default_domains = [
-    "aws.amazon.com",
-    "google.com",
-    "yandex.ru",
-    "google.com",
-    "youtube.com",
-    "linkedin.com",
-    "github.com",
-    "ubuntu.com",
-    "debian.org",
-    "linuxmint.com",
-    "linkedin.com",
-    "microsoft.com",
-    "google.com",
-    "linux.org.ru",
-    "meduza.io",
-    "echo.msk.ru",
-    "example.com",
-    "github.com",
-    "flw.im",
-    "o-nix.com",
-    "kubernetes.io",
-    "cloud.google.com",
-    "python.org",
-    "docs.python.org",
-    "wikipedia.org",
-    "en.wikipedia.org",
-    "bottlepy.org",
-    "hub.docker.com",
-    "docker.com",
-    "pypi.org",
-    "stackoverflow.com",
-    "realpython.com",
-    "medium.com",
-    "linux.org",
-    "notexisting.domain",
-]
 
 r = redis.Redis(host=redis_host)
 
@@ -103,8 +72,7 @@ def update_domains_list_from_file():
         return my_list
     except Exception as e:
         print("Exception during to open domain list file", str(e))
-        print("Using the default domain list")
-        return default_domains
+        return []
 
 
 def is_redis_available():
@@ -124,14 +92,14 @@ def decode_redis_value(value):
 
 
 def update_all_domains_in_redis(domains_set):
-    if is_redis_available():
+    if is_redis_available() and domains_set:
         if len(domains_set) > 50:
             domains_chunk_size = int(len(domains_set) / 10)
         else:
             domains_chunk_size = len(domains_set)
         begin = 0
         while begin < len(domains_set):
-            sub_set = set(list(domains_set)[begin : begin + domains_chunk_size])
+            sub_set = set(list(domains_set)[begin: begin + domains_chunk_size])
             pool = Pool(len(sub_set))
             for result_tuple in pool.imap_unordered(
                 ssl.tuple_domain_unixtime_expiration, sub_set
@@ -143,7 +111,8 @@ def update_all_domains_in_redis(domains_set):
                 ):
                     r.hset(
                         name=result_tuple[0],
-                        mapping={"exp": result_tuple[1], "updated": round(time.time())},
+                        mapping={"exp": result_tuple[1],
+                                 "updated": round(time.time())},
                     )
             print("Updated info for domains: {}".format(str(sub_set)))
             time.sleep(1)
@@ -155,7 +124,8 @@ def update_outdated_info_in_redis():
         from_redis_set = set()
         for domain in domains_set:
             if (not r.hget(domain, "exp")) or (
-                round(time.time()) - decode_redis_value(r.hget(domain, "updated"))
+                round(time.time()) -
+                decode_redis_value(r.hget(domain, "updated"))
                 > seconds_between_info_updates
             ):
                 from_redis_set.add(domain)
@@ -187,7 +157,8 @@ def update_domains_if_md5_changed():
 if is_hash_sum_check_enabled:
     md5_hash = domains_file_md5()
 
-domains_set = set(update_domains_list_from_file())
+if is_first_load_from_file_enabled:
+    domains_set = set(update_domains_list_from_file())
 
 while not is_redis_available():
     print("Waiting for the Redis during the startup...")
