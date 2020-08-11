e6f9876b30f9e6b9f94b0b0371c6fb998a49c966
Dmitrii Makarov <vensder@gmail.com>
2020-08-11 18:49:54 +1200

    updated diagram

--- img/diagrams/dac.py
+++ img/diagrams/dac.py
@@ -6,7 +6,12 @@ from diagrams.onprem.container import Docker
 from diagrams.onprem.inmemory import Redis
 from diagrams.onprem.network import Internet
 
-with Diagram("SSL-checker-diagram", show=False):
+graph_attr = {
+    "fontsize": "18",
+    "pad": "0"
+}
+
+with Diagram("SSL-checker-diagram", show=False, graph_attr=graph_attr):
     Internet("Internet") >> ELB("ingress") >> [
             Docker("web-app"), 
             Docker("web-app"),
