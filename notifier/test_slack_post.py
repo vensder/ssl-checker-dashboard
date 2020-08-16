1a4df768355dc2fcab2dbe71f8782da9098fc08f
Dmitrii Makarov <vensder@gmail.com>
2020-08-16 16:28:56 +1200

    fixed test

--- notifier/test_slack_post.py
+++ notifier/test_slack_post.py
@@ -2,6 +2,6 @@ import unittest
 import slack_post as sp
 
 class CheckResponseCode(unittest.TestCase):
-    def check_response_code(self):
+    def test_response_code(self):
         self.assertEqual(sp.post_message(sp.webhook_url, "example.com", 5), 200)
 
