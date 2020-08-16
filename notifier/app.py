3e6ca9914496862a317371cc0fcda2ee2e98f3cb
Dmitrii Makarov <vensder@gmail.com>
2020-08-16 23:28:35 +1200

    added docker for notifier

--- notifier/app.py
+++ notifier/app.py
@@ -9,22 +9,22 @@ from datetime import datetime
 from distutils.util import strtobool
 
 
-
 redis_host = "redis"
 if "REDIS_HOST" in environ and environ["REDIS_HOST"]:
     redis_host = environ["REDIS_HOST"]
 
 notify_if_days_left = 30
 if "NOTIFY_IF_DAYS_LEFT" in environ and environ["NOTIFY_IF_DAYS_LEFT"]:
-    notify_if_days_left = environ["NOTIFY_IF_DAYS_LEFT"]
+    notify_if_days_left = int(environ["NOTIFY_IF_DAYS_LEFT"])
 
 notify_every_n_hours = 0.5
 if "NOTIFY_EVERY_N_HOURS" in environ and environ["NOTIFY_EVERY_N_HOURS"]:
-    notify_every_n_hours = environ["NOTIFY_EVERY_N_HOURS"]
+    notify_every_n_hours = float(environ["NOTIFY_EVERY_N_HOURS"])
+
 
 def send_notification(host, days):
     response_code = post_message(webhook_url, host, days)
-    print(response_code)
+    return response_code
 
 
 def is_redis_available():
@@ -53,11 +53,14 @@ def notify_expiring_soon():
                         days = round(
                             (int(expire_unix_time) - datetime.now().timestamp()) / 86400
                         )
-                        
-                        if not r.hget(host, "notified") or not strtobool(r.hget(host, "notified").decode("utf-8")):
+
+                        if not r.hget(host, "notified") or not strtobool(
+                            r.hget(host, "notified").decode("utf-8")
+                        ):
                             print(f"SSL cert of {host} will expire in {days} day(s)")
                             send_notification(host, days)
                             r.hset(host, "notified", "True")
+
 
 def delete_notified_mark():
     if is_redis_available():
@@ -65,10 +68,11 @@ def delete_notified_mark():
             if r.hget(host, "notified"):
                 r.hset(host, "notified", "False")
 
+
 if __name__ == "__main__":
     r = redis.Redis(host=redis_host)
 
-    schedule.every(5).seconds.do(notify_expiring_soon)
+    schedule.every(10).seconds.do(notify_expiring_soon)
     schedule.every(notify_every_n_hours).hours.do(delete_notified_mark)
     while True:
         schedule.run_pending()
