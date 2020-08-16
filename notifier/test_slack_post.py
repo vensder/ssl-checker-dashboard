110a1a3eb3b626bb5d1b67a0b88df3b13aee69d0
Dmitrii Makarov <vensder@gmail.com>
2020-08-16 16:35:12 +1200

    added secret env var to pipeline

--- notifier/test_slack_post.py
+++ notifier/test_slack_post.py
@@ -1,7 +1,9 @@
 import unittest
 import slack_post as sp
 
+webhook_url = environ["secrets.SLACK_WEBHOOK_URL"]
+
 class CheckResponseCode(unittest.TestCase):
     def test_response_code(self):
-        self.assertEqual(sp.post_message(sp.webhook_url, "example.com", 5), 200)
+        self.assertEqual(sp.post_message(webhook_url, "example.com", 5), 200)
 
