9d762a6f9f57f57e2571ad005f6c6c838e5fa300
Dmitrii Makarov <vensder@gmail.com>
2020-08-17 21:56:00 +1200

    removed unneeded import

--- img/diagrams/dac.py
+++ img/diagrams/dac.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 from diagrams import Diagram, Cluster
-from diagrams.aws.network import ELB
 from diagrams.onprem.container import Docker
 from diagrams.onprem.inmemory import Redis
 from diagrams.onprem.network import Internet, Nginx
 from diagrams.saas.chat import Slack
-from diagrams.onprem.compute import Server
 from diagrams.onprem.client import User
 
 
